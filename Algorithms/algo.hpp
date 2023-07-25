ae26648722dfe8f28f09fdde8b5e88e707b79ea0
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-07-25 07:39:54 -0700

    d

--- Algorithms/algo.hpp
+++ Algorithms/algo.hpp
@@ -1,50 +1,50 @@
 #pragma ONCE
-#include <vector>
 #include <iostream>
 #include <queue>
+#include <vector>
 using namespace std;
 class Solution {
 public:
-    int removeDuplicates(vector<int>& nums) {
-        // use queue and loop through entire array??
-        std::queue<int> q;
-        vector<int> ans;
-        int fix=0;
-        while(fix<nums.size()){
-        bool flag=false;
-        q.push(nums[fix]);
-        for(int i=fix+1;i<nums.size();i++){
-            if (q.front()==nums[i]){
-                // duplicate, needs to be removed from vector
-                flag=true;
-                 break;
-            }
+  int removeDuplicates(vector<int> &nums) {
+    // use queue and loop through entire array??
+    std::queue<int> q;
+    vector<int> ans;
+    int fix = 0;
+    while (fix < nums.size()) {
+      bool flag = false;
+      q.push(nums[fix]);
+      for (int i = fix + 1; i < nums.size(); i++) {
+        if (q.front() == nums[i]) {
+          // duplicate, needs to be removed from vector
+          flag = true;
+          break;
         }
-        if(flag != true){
-            // is NOT a duplicate
-            // take out of queue
-            q.pop();
-ans.push_back(nums[fix]);
-                fix++;
-        }
-        else{
-            // IS a duplicate, dont add to ans, leave in queue
-           continue;
-        }
-        }
+      }
+      if (flag != true) {
+        // is NOT a duplicate
+        // take out of queue
+        q.pop();
+        ans.push_back(nums[fix]);
+        fix++;
+      } else {
+        // IS a duplicate, dont add to ans, leave in queue
+        continue;
+      }
+    }
     return ans.size();
-    }
+  }
 
-    int removeDuplicatesconstant(vector<int>& nums) {
-        // bubble sort method
-   int index=0;
-// sorts using a fixed in-place variable index that iterates up to store non-duplicates
-        for(int y=1;y<nums.size();y++){
-            if(nums[y]!=nums[y-1]){
-              nums[index+1]=nums[y];
-              index++;
-            }
-        }
-        return index+1;
+  int removeDuplicatesconstant(vector<int> &nums) {
+    // bubble sort method
+    int index = 0;
+    // sorts using a fixed in-place variable index that iterates up to store
+    // non-duplicates
+    for (int y = 1; y < nums.size(); y++) {
+      if (nums[y] != nums[y - 1]) {
+        nums[index + 1] = nums[y];
+        index++;
+      }
     }
+    return index + 1;
+  }
 };
