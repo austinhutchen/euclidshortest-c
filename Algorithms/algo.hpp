ace48ce25be845efdb5e5cd6972fda0b380b4e3b
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-08-08 06:26:48 -0700

    d

--- Algorithms/algo.hpp
+++ Algorithms/algo.hpp
@@ -304,5 +304,14 @@ public:
     }
   }
 
+  int fib(int n) {
+    if (n == 0 || n == 1) {
+      return n;
+    } else {
+      n = fib(n - 1) + fib(n - 2);
+    }
+    return n;
+  }
+
 private:
 };
