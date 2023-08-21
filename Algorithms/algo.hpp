28b8f2d67ea415bdf866d49659805acb479b3298
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-08-21 06:00:11 -0700

    bsrch

--- Algorithms/algo.hpp
+++ Algorithms/algo.hpp
@@ -535,4 +535,22 @@ public:
     }
     return true;
   }
+
+  bool isBadVersion( int i){
+    return true;
+  }
+  int firstBadVersion(int n) {
+    if (n == 1)
+      return n;
+
+    int low = 1, high = n;
+    while (low < high) {
+      int mid = low + (high - low) / 2;
+      if (isBadVersion(mid))
+        high = mid;
+      else
+        low = mid + 1;
+    }
+    return low;
+  }
 };
