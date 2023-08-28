45cbf5aa238668b82e6dd6e5dbd29b753a7dce7c
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-08-28 11:39:22 -0700

    mxn matrix srch

--- Algorithms/algo.hpp
+++ Algorithms/algo.hpp
@@ -31,6 +31,24 @@ class Solution {
 public:
   Solution() {}
 
+  bool searchMatrix(vector<vector<int> > &matrix, int target) {
+    int n = matrix.size();
+    int m = matrix[0].size();
+    int low = 0, high = n * m - 1, mid, row, col;
+    while (low <= high) {
+      mid = (low + high) / 2;
+      row = mid / m; // getting the row number
+      col = mid % m; // getting the column number
+      if (matrix[row][col] == target)
+        return true;
+      else if (matrix[row][col] > target)
+        high = mid - 1;
+      else
+        low = mid + 1;
+    }
+    return false;
+  }
+
   int hammingWeight(unsigned n) {
     int counter = 0;
     while (n > 0) {
