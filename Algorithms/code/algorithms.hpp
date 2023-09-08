b43a72fab4ad269ae7a374b5868467634aec16bc
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-09-08 07:19:54 -0700

    pointer arithmetic instead of 2D vector for optimizations

--- Algorithms/code/algorithms.hpp
+++ Algorithms/code/algorithms.hpp
@@ -1,4 +1,5 @@
 #include <cstdlib>
+#include <sys/_types/_size_t.h>
 #pragma ONCE
 #include <fstream>
 #include <iostream>
@@ -7,22 +8,48 @@ using namespace std;
 
 struct coordinate {
   // x is a vector in R^2
-  double x[2] ={0.0,0.0};
-  coordinate(double x3, double x4) {
-    setcoords(x3, x4);
+  void setcoord(const char *x1) {
+    if(x1==nullptr){
+      return;
+    }
+    if (!f1 && !f2) {
+      x[0] = atof(x1);
+      f1 = true;
+    }
+    if (!f1 && f2) {
+      x[0] = atof(x1);
+      f1 = true;
+    }
+    if (!f2 && f1) {
+      x[1] = atof(x1);
+      f2 = true;
+    }
   }
-  void setcoords(double x1, double x2) {
-    this->x[0] = x1;
-    this->x[1]= x2;
+  double x0(void) { return x[0]; }
+  double x1(void) { return x[1]; }
+  bool isfull(void) {
+    if (f1 && f2) {
+      return true;
+    }
+    return false;
   }
   float distance(float y1, float y2) {
     // Calculating distance from y1 and y2
     return sqrt(pow(this->x[1] - this->x[0], 2) + pow(y2 - y1, 2) * 1.0);
   }
+  void printcoords(void) {
+    cout << "( " << *x << " , " << *(x + 8) << " )" << endl;
+  }
+
+private:
+  bool f1, f2;
+  double x[2];
+  size_t size;
 };
 // Assignment 1
 class PlaneArithmetic {
 public:
+  // HELPERS
   char *find(string str, char a, int start) {
     char *t = &str[start];
     while (t != '\0') {
@@ -33,87 +60,27 @@ public:
     }
     return nullptr;
   }
-  void printvec(vector<vector<double> > nums) {
+  void printvec(vector<coordinate **> nums) {
     for (int i = 0; i < nums.size(); i++) {
-      for (int k = 0; k < nums[i].size(); k++) {
-        cout << nums[i][k] << " , " ;
-      }
-    }
-    cout <<endl;
-  }
-    // for CECS
-    void numparse(string in) {
-
-      // start AFTER first bracket
-
-      int index = 1;
-      ifstream f;
-      const char *ind;
-      f.open(in);
-      vector<vector<double> > nums;
-      string line;
-
-      if (!f.fail()) {
-        vector<double> buffarr;
-        while (getline(f, line)) {
-          if(line!=""){
-          buffarr.clear();
-          int count=0;
-          for (char *i = &line[0]; *i != '#'; i++) {
-                  string flt;
-            while (*i <= 57 && *i>47) {
-
-              // float read
-              flt += char(*i);
-              i++;
-            }
-            
-          
-              const char *buffer = flt.c_str();
-              double answer = atof(buffer);
-              // nums.push_back(strtod(&flt[0], &flt));
-              buffarr.push_back(answer);
-            
-          }
-          nums.push_back(buffarr);
+      coordinate *p = nums[i][0];
+      unsigned short counter = 0;
+      do {
+        if (p != 0x0) {
+          p->printcoords();
+          p += sizeof(coordinate);
+          counter++;
         }
-        // float parse
-      }
-      int *p2 = NULL;
-      twoDimBubbleSort(nums, 0, 0);
-      printvec(nums);
-      //asdfsd
-      return ;
-
+      } while (counter < 10);
     }
-  
-    }
-
-  double *closestpair(vector<vector<double> > nums) {
-    // determine closest pair of points in plane represented by distance
-    // function
-    double vals[] = {0.0, 0.0};
-    coordinate *current = new coordinate(0, 0);
-    // return the pair as nums
-    // sort nums
-    // should now be sorted in terms of euclidian plane
-    // split nums into left and right halves using algorithm
-
-    vector<double> f(nums[0].size());
-    for(int i=0;i<nums.size();i++){
-      vector<double> left = nums[0]; //closest points to assigned midpoint in left line
-       vector<double> right= nums[nums.size()] ; //closest points to assigned midpoint in right line
-              vector<double> mid= nums[nums.size()/2] ; 
-    }
-
-    return vals;
+    cout << endl;
   }
-  void swap(double *a, double *b) {
-    int temp = *b;
+  void swap(coordinate **a, coordinate **b) {
+    coordinate *temp = *b;
     *b = *a;
     *a = temp;
   }
-  void twoDimBubbleSort(vector<vector<double> > nums, int row, int col) {
+  // adjust for sorting by coordinates
+  void twoDimBubbleSort(vector<coordinate **> nums, int row, int col) {
     // REALLY inefficient but is easy to read
     for (int i = 0; i < (row * col); ++i) {
       for (int j = 0; j < (row * col) - 1; ++j) {
@@ -122,14 +89,89 @@ public:
         int nr = (j + 1) / col; // next item row
         int nc = (j + 1) % col; // next item column
 
-        if (nums[cr][cc] > nums[nr][nc]){
-                     swap(&nums[cr][cc],
-                     &nums[nr][nc]); // any way you want to swap variables
+        if (nums[cr][cc]->x0() > nums[nr][nc]->x0()) {
+          swap(&nums[cr][cc],
+               &nums[nr][nc]); // any way you want to swap variables
         }
-       
       }
     }
   }
+  // END HELPERS
+  //  - -- - --- -- - - - - - - -- - -------------------->               FOR
+  //  CECS            < ------ - - - - - - - - - -- - - - - -- - - -
+  void numparse(string in) {
+
+    // start AFTER first bracket
+
+    int index = 1;
+    ifstream f;
+    const char *ind;
+    f.open(in);
+    // vector of rows of arrays of max size 20 representing coordinates
+    vector<coordinate **> nums;
+    string line;
+    // array of "window" of coordinate pointers being compared at a time
+    // max of 10 pairs of points per line.
+    coordinate *points[10] = {0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0};
+    unsigned counter;
+    if (!f.fail()) {
+      while (getline(f, line)) {
+        if (!line.empty()) {
+          counter = 0;
+          for (char *i = &line[index]; *i != '#'; i++) {
+            string flt;
+
+            while (*i <= 57 && *i > 47) {
+
+              // float read
+              flt += char(*i);
+              i++;
+            }
+
+            switch (*i) {
+            case 123: {
+              // open bracket
+              points[counter] = new coordinate();
+            }
+            // close bracket;
+            case ',': {
+              // comma 
+              const char *buffer = flt.c_str();
+              points[counter]->setcoord(buffer);
+              flt="";
+            }
+            case 125:{
+              if (points[counter]->isfull()) {
+                counter++;
+              }
+              // closing bracket
+            }
+              // will insert newline char at end of string
+              // double answer = atof(buffer);
+              //  temp.push_back(buffer)
+              // each point in array represents a pair of coordinates
+            }
+            // nums.push_back(points);
+            nums.push_back(points);
+          }
+          // float parse
+        }
+        printvec(nums);
+        // asdfsd
+        return;
+      }
+    }
+  }
+
+  double *closestpair(vector<coordinate **> nums) {
+    // determine closest pair of points in plane represented by distance
+    // function
+    double temp[] = {0.0, 0.0};
+    // temp used for nearest pair of points
+    // return the pair as nums
+    // sort nums
+    // should now be sorted in terms of euclidian plane
+    // split nums into left and right halves using algorithm
+    return temp;
+  }
 };
-
-
